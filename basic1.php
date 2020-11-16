1e4a6940fcdac95868617afa4a438d928a10aee4
Kimberley Blankenhorn <kvandlen@yahoo.com>
2020-11-16 14:27:19 +0100

    expanding on basic

--- basic1.php
+++ basic1.php
@@ -24,5 +24,37 @@ class Cars
 }
 $myCars = new Cars( 'Audi', 'A5', '2020', 'black', 'diesel');
 $auto = $myCars->addCars();
-print  "You have $auto";
+print  "You have $auto <br>";
+
+class Ships
+{
+  public $name;
+  public $model;
+  public $year;
+  public $color;
+  public $fuelType;
+  public $size;
+  public $shippingType;
+
+
+
+  function __construct($name_arg, $model_arg, $year_arg, $color_arg, $fuelType_arg, $size_arg, $shippingType_arg)
+  {
+      $this->name   = $name_arg;
+      $this->model  = $model_arg;
+      $this ->year  = $year_arg;
+      $this ->color  = $color_arg; 
+      $this ->fuelType  = $fuelType_arg;
+      $this ->size = $size_arg;
+      $this ->shippingType = $shippingType_arg;
+  }
+  public function addShips ()
+  {
+      $totalShips = $this->name . " " . $this->model . " " . $this ->year . " " . $this ->color . " with " . $this ->fuelType . " " . $this ->size . " " . $this ->shippingType;
+      return $totalShips;
+  }
+}
+$myShips = new Ships( 'Aida', '11223', '2020', 'White', 'diesel', 'big', 'passenger');
+$ships = $myShips->addShips();
+print  "You have $ships";
 ?>
